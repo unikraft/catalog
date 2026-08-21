2816af4f85ddec43bf9a27e5242e9d3fff9a218b
Daniel-Ioan Dinu <daniel_dinu30@yahoo.com>
2026-08-21 14:14:17 +0300

    feat(examples): Add distroless Rust Actix Web4 example
    Use cc-debian12 distroless image for full compatibility
    with bookworm build. It also includes libc, libm, libgcc_s
    and standard dynamic linkers.
    
    Additionally, use SHA digest for distroless image.
    
    Signed-off-by: Daniel-Ioan Dinu <daniel_dinu30@yahoo.com>

---
+++ examples/actix4-rust1.75-distroless/src/main.rs
@@ -0,0 +1,29 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Bye, World!\n")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Buh bye!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/hey", web::get().to(manual_hello))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
+
